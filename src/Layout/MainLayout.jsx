f122abcbe90f9a15a50e211243e6708f0a1adc3e
Arijit <arijitbasubd@gmail.com>
2024-12-03 20:51:37 +0600

    theme switch

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import Header from '../Components/Header';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Header from "../Components/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer";
 
 const MainLayout = () => {
-    return (
-        <div className='dark:bg-black dark:text-white'>
-            <div className="container mx-auto py-5"><Header></Header></div>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div className="dark:bg-slate-900 dark:text-white relative h-screen">
+      <div className="container mx-auto py-5">
+        <Header></Header>
+      </div>
+      <Outlet></Outlet>
+      <div className="absolute bottom-0 w-full">
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
